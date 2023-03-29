d7f2cc41cb78ab2a002a5a38075e9dbd6b65d037
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-03-28 21:22:24 -0400

    testing

--- util/update_codegen.sh
+++ util/update_codegen.sh
@@ -8,16 +8,10 @@ gen_ver=$( awk '/k8s.io\/code-generator/ { print $2 }' "$rootdir/go.mod" )
 GOPATH=$(go env GOPATH)
 codegen_pkg=${GOPATH}/pkg/mod/k8s.io/code-generator@${gen_ver}
 
-
-ROOT_PACKAGE="https://"github.com/UWNetworksLab/app-defined-networks"
-
-crds=(adnconfig:v1alpha)
-
 chmod +x "${codegen_pkg}/generate-groups.sh"
 
-GO111MODULE='on' "${codegen_pkg}/generate-groups.sh" \
-  'deepcopy,client,informer,lister' \
-  "${ROOT_PACKAGE}/controller/pkg/client" \
-  "${ROOT_PACKAGE}/controller/pkg/apis" \
-  "${crds[*]}" \
-  --go-header-file "${codegen_pkg}"/hack/boilerplate.go.txt
+"${codegen_pkg}/generate-groups.sh" "deepcopy,client,informer,lister" \
+  "github.com/UWNetworksLab/app-defined-networks/controller/gen/generated" \
+  "github.com/UWNetworksLab/app-defined-networks/controller/gen/apis" \
+  adncontroller:v1alpha \
+  --go-header-file $utildir/boilerplate.go.txt
