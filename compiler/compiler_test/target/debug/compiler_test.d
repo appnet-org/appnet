4ee18c28887be6d2980d6d116af6cc16a66fdc79
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-05-26 22:58:52 -0700

    add more compiler code

--- compiler/compiler_test/target/debug/compiler_test.d
+++
@@ -1,1 +0,0 @@
-/Users/xzhu/Desktop/service_mesh/app-defined-networks/compiler/compiler_test/target/debug/compiler_test: /Users/xzhu/Desktop/service_mesh/app-defined-networks/compiler/compiler_test/src/main.rs
