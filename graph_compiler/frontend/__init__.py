22f56fd7452c5931bded599544b318ab87d77f99
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-07-27 12:54:36 -0400

    feat: graphspec grammar

--- graph_compiler/frontend/__init__.py
+++ graph_compiler/frontend/__init__.py
@@ -2,10 +2,11 @@ import re
 
 from graph_compiler.frontend.parser import GraphParser
 
+
 class GraphCompiler:
     def __init__(self):
         self.parser = GraphParser()
-    
+
     def compile(self, spec: str):
         # parsing
         ast = self.parser.parse(spec)
