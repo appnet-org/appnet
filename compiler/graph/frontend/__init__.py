abd3abf277fea8f19a9623c23ebf88156cee7396
Yuyao Wang <yuyaowang_kristoff@outlook.com>
2023-10-31 13:14:00 -0600

    refactor: remove req/res elements in parser

--- compiler/graph/frontend/__init__.py
+++ compiler/graph/frontend/__init__.py
@@ -12,10 +12,8 @@ class GCParser:
     def __init__(self):
         self.services = set()
         self.app_edges = []
-        self.req_graphir: Dict[str, GraphIR] = dict()
-        self.res_graphir: Dict[str, GraphIR] = dict()
 
-    def parse(self, spec_path: str) -> Dict[str, GraphIR]:
+    def parse(self, spec_path: str) -> Tuple[Dict[str, GraphIR], Dict[str, str]]:
         with open(spec_path, "r") as f:
             spec_dict = yaml.safe_load(f)
         for edge in spec_dict["app_structure"]:
@@ -28,17 +26,17 @@ class GCParser:
         for client, server in self.app_edges:
             chain, pair, eid = [], [], f"{client}->{server}"
             # client's egress
-            if client in spec_dict["egress"]:
+            if "egress" in spec_dict and client in spec_dict["egress"]:
                 chain.extend(spec_dict["egress"][client])
             # client->server edge
-            if f"{client}->{server}" in spec_dict["edge"]:
+            if "edge" in spec_dict and f"{client}->{server}" in spec_dict["edge"]:
                 chain.extend(spec_dict["edge"][eid])
             # server's ingress
-            if server in spec_dict["ingress"]:
+            if "ingress" in spec_dict and server in spec_dict["ingress"]:
                 chain.extend(spec_dict["ingress"][server])
             # client->server link
-            if f"{client}->{server}" in spec_dict["link"]:
+            if "link" in spec_dict and f"{client}->{server}" in spec_dict["link"]:
                 pair.extend(spec_dict["link"][eid])
             if len(chain) + len(pair) > 0:
                 graphir[eid] = GraphIR(client, server, chain, pair)
-        return graphir
+        return graphir, spec_dict["service_pos"]
