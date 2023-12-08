3cf05893952f563d93bc4215ea717e24f69a78d9
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-12-08 06:01:12 +0000

    Add mutation and logging elements

--- envoy/elements/logging/src/lib.rs
+++ envoy/elements/logging/src/lib.rs
@@ -1,31 +1,27 @@
 use proxy_wasm::traits::{Context, HttpContext};
 use proxy_wasm::types::{Action, LogLevel};
-use std::sync::atomic::{AtomicUsize, Ordering};
 
 use prost::Message;
 pub mod ping {
     include!(concat!(env!("OUT_DIR"), "/ping_pb.rs"));
 }
 
-static GLOBAL_COUNTER: AtomicUsize = AtomicUsize::new(0);
-
 #[no_mangle]
 pub fn _start() {
     proxy_wasm::set_log_level(LogLevel::Trace);
     proxy_wasm::set_http_context(|context_id, _| -> Box<dyn HttpContext> {
-        Box::new(AccessControl { context_id, abort_count: 0 })
+        Box::new(Logging { context_id })
     });
 }
 
-struct AccessControl {
+struct Logging {
     #[allow(unused)]
     context_id: u32,
-    abort_count: u32,
 }
 
-impl Context for AccessControl {}
+impl Context for Logging {}
 
-impl HttpContext for AccessControl {
+impl HttpContext for Logging {
     fn on_http_request_headers(&mut self, _num_of_headers: usize, end_of_stream: bool) -> Action {
         log::warn!("executing on_http_request_headers");
         if !end_of_stream {
@@ -49,24 +45,9 @@ impl HttpContext for AccessControl {
             // Parse grpc payload, skip the first 5 bytes
             match ping::PingEchoRequest::decode(&body[5..]) {
                 Ok(req) => {
-                    // log::info!("req: {:?}", req);
-                    // log::warn!("body.len(): {}", req.body.len());
-                    // log::warn!("body : {}", req.body);
-                    if req.body == "test" {
-                        self.abort_count += 1; // Increment the counter
-                        // log::warn!("Aborting a request!!!! Abort Count: {}", self.abort_count);
-                        let counter_val = GLOBAL_COUNTER.fetch_add(1, Ordering::SeqCst) + 1;
-                        log::warn!("Global counter value: {}", counter_val);
-                        self.send_http_response(
-                            200,
-                            vec![
-                                ("grpc-status", "1"),
-                                // ("grpc-message", "Access forbidden.\n"),
-                            ],
-                            None,
-                        );
-                        return Action::Pause;
-                    }
+                    log::info!("req: {:?}", req);
+                    log::warn!("body.len(): {}", req.body.len());
+                    log::warn!("body : {}", req.body);
                 }
                 Err(e) => log::warn!("decode error: {}", e),
             }
--- envoy/elements/wasm-template/build.rs
+++
@@ -1,6 +0,0 @@
-const PROTO: &str = "../pb/echo.proto";
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed={PROTO}");
-    prost_build::compile_protos(&[PROTO], &["../pb"])?;
-    Ok(())
-}
