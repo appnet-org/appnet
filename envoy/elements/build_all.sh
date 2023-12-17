2deb0e016fdbe65867f685b6e54acd4cc1ab4178
banruo <banruo@cs.washington.edu>
2023-12-17 12:00:18 +0000

    update .gitignore and script

---
+++ envoy/elements/build_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Iterate over each item in the current directory
+for folder in */ ; do
+    # Check if it's a directory
+    if [ -d "$folder" ]; then
+        # Change directory to the folder
+        cd "$folder"
+
+        # Check if build.sh exists and is executable
+        if [ -x "build.sh" ]; then
+            # Run the build.sh script
+            ./build.sh
+        else
+            echo "build.sh not found or not executable in $folder"
+        fi
+
+        # Go back to the parent directory
+        cd ..
+    fi
+done
