3870a55b35d5b47660e19e93ced99551a48a2872
xzhu <xzhu@localhost>
2024-06-17 21:14:47 -0400

    update go version

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@
 if ! command -v go &> /dev/null
 then
     echo "Go not found. Installing Go"
-    wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
-    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
+    wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
 
     echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
     source ~/.bashrc
-    rm go1.22.1.linux-amd64.tar.gz
+    rm go1.22.2.linux-amd64.tar.gz
 fi
 
 set -e
