0d71998affeab29d83395eb2f35b6e21da06253c
Xiangfeng Zhu <xzhu0027@gmail.com>
2024-08-13 02:00:42 -0400

    update service router and praquel

--- utils/shardmanager/build_images.sh
+++ utils/shardmanager/build_images.sh
@@ -4,9 +4,9 @@ set -ex
 sudo docker build --tag shard-manager:latest -f Dockerfile .
 
 # Tag the images
-sudo docker tag shard-manager xzhu0027/shard-manager:latest
+sudo docker tag shard-manager <docker-username>/shard-manager:latest
 
 # Push the images to the registry
-sudo docker push  xzhu0027/shard-manager:latest
+sudo docker push  <docker-username>/shard-manager:latest
 
 set +ex
