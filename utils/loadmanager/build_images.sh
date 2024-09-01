a164c0704453e80c35f192ef37da5cb621eaa8dc
Xiangfeng Zhu <xzhu0027@gmail.com>
2024-09-01 02:57:34 -0400

    update docker usernames

--- utils/loadmanager/build_images.sh
+++ utils/loadmanager/build_images.sh
@@ -4,9 +4,9 @@ set -ex
 sudo docker build --tag load-manager:latest -f Dockerfile .
 
 # Tag the images
-sudo docker tag load-manager xzhu0027/load-manager:latest
+sudo docker tag load-manager appnetorg/load-manager:latest
 
 # Push the images to the registry
-sudo docker push  xzhu0027/load-manager:latest
+sudo docker push  appnetorg/load-manager:latest
 
 set +ex
--- utils/shardmanager/build_images.sh
+++ utils/shardmanager/build_images.sh
@@ -4,9 +4,9 @@ set -ex
 sudo docker build --tag shard-manager:latest -f Dockerfile .
 
 # Tag the images
-sudo docker tag shard-manager xzhu0027/shard-manager:latest
+sudo docker tag shard-manager appnetorg/shard-manager:latest
 
 # Push the images to the registry
-sudo docker push  xzhu0027/shard-manager:latest
+sudo docker push  appnetorg/shard-manager:latest
 
 set +ex
