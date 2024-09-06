469b12eea5cb665869cf7b7f0a8bc7b2eed30929
jokerwyt <914554688@qq.com>
2024-09-05 23:28:55 -0600

    istio ambient imagePullPolicy

--- utils/istio_setup_ambient.sh
+++ utils/istio_setup_ambient.sh
@@ -8,7 +8,7 @@ pushd istio-*
 sudo cp bin/istioctl /usr/local/bin
 kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
   { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v1.0.0" | kubectl apply -f -; }
-istioctl install --set profile=ambient --set "components.ingressGateways[0].enabled=true" --set "components.ingressGateways[0].name=istio-ingressgateway" --skip-confirmation --set hub=docker.io/appnetorg
+istioctl install --set profile=ambient --set "components.ingressGateways[0].enabled=true" --set "components.ingressGateways[0].name=istio-ingressgateway" --skip-confirmation --set hub=docker.io/appnetorg --set "values.global.imagePullPolicy=Always"
 popd
 
 set +ex
